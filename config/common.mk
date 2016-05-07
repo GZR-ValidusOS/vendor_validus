wifiboy/vendor_tipsy

Add Viper4Android
Change-Id: I33d75ded25b892e99f435748d344bc6edf30d19f
Signed-off-by: Martinusbe <martinusbe@gmail.com>

config/common.mk
@@ -120,6 +120,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/tipsy/overlay/common
 
+# Viper4Android
+PRODUCT_COPY_FILES += \
+   vendor/tipsy/prebuilt/common/bin/audio_policy.sh:system/audio_policy.sh \
+   vendor/tipsy/prebuilt/common/addon.d/95-LolliViPER.sh:system/addon.d/95-LolliViPER.sh \
+   vendor/tipsy/prebuilt/common/su.d/50viper.sh:system/su.d/50viper.sh \
+   vendor/tipsy/prebuilt/common/app/Viper4Android/Viper4Android.apk:system/priv-app/Viper4Android/Viper4Android.apk 
+
 # Bootanimation 
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip

GZR-ValidusOS/vendor_validus

Conflicts:
	config/common.mk

config/common.mk
@@ -134,6 +134,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/tesla/overlay/common
 
+# Viper4Android
+PRODUCT_COPY_FILES += \
+   vendor/tesla/prebuilt/common/bin/audio_policy.sh:system/audio_policy.sh \
+   vendor/tesla/prebuilt/common/addon.d/95-LolliViPER.sh:system/addon.d/95-LolliViPER.sh \
+   vendor/tesla/prebuilt/common/su.d/50viper.sh:system/su.d/50viper.sh \
+   vendor/tesla/prebuilt/common/app/Viper4Android/Viper4Android.apk:system/priv-app/Viper4Android/Viper4Android.apk
+
 # Boot animation include
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 