27ce0c5c2424739b138e1994fcdb6d617da988c1
Steve Kondik <steve@cyngn.com>
2016-08-07 23:21:29 -0700

    cm: Add custom off-mode charging screen
      * Moving this to open-source from CyanogenOS :)
      * Thanks to Chao Chen, Ethan Chen, Pat Erley, Scott Mertz,
        and Keith Mok for various contributions to this.
    
    Change-Id: If79f9c279668d14ee0cde62889bb09e7185ef08c
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	config/common.mk

--- config/common.mk
+++ config/common.mk
@@ -108,6 +108,15 @@ PRODUCT_PACKAGES += \
     ntfsfix \
     ntfs-3g
 
+# Custom off-mode charger
+ifneq ($(WITH_CM_CHARGER),false)
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    cm_charger_res_images \
+    font_log.png \
+    libhealthd.cm
+endif
+
 # DU Utils Library
 PRODUCT_BOOT_JARS += \
     org.dirtyunicorns.utils
