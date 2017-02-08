67fda8cb1408216bc2aa6c8f93f3343c030c1dfa
Martinusbe <martinusbe@gmail.com>
2017-02-08 16:15:29 +0100

    fix up qcom_target.mk
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -25,10 +25,16 @@ $(call project-set-path,qcom-$(2),$(strip $(path)))
 endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+    B_FAMILY := msm8226 msm8610 msm8974
+    B64_FAMILY := msm8992 msm8994
+    BR_FAMILY := msm8909 msm8916
+    UM_FAMILY := msm8937 msm8953
 
     qcom_flags := -DQCOM_HARDWARE
     qcom_flags += -DQCOM_BSP
     qcom_flags += -DQTI_BSP
+
+    BOARD_USES_ADRENO := true
 
     TARGET_USES_QCOM_BSP := true
 
@@ -61,50 +67,32 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
     2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
-    ifeq ($(QCOM_HARDWARE_VARIANT),)
-        ifneq ($(filter msm8610 msm8226 msm8974,$(TARGET_BOARD_PLATFORM)),)
-            QCOM_HARDWARE_VARIANT := msm8974
-        else
-        ifneq ($(filter msm8909 msm8916,$(TARGET_BOARD_PLATFORM)),)
-            QCOM_HARDWARE_VARIANT := msm8916
-        else
-        ifneq ($(filter msm8953 msm8937,$(TARGET_BOARD_PLATFORM)),)
-            QCOM_HARDWARE_VARIANT := msm8937
-        else
-        ifneq ($(filter msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
-            QCOM_HARDWARE_VARIANT := msm8994
-        else
-            QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
-        endif
-        endif
-        endif
-        endif
+    ifeq ($(call is-board-platform-in-list, $(B_FAMILY)),true)
+        MSM_VIDC_TARGET_LIST := $(B_FAMILY)
+        QCOM_HARDWARE_VARIANT := msm8974
+    else
+    ifeq ($(call is-board-platform-in-list, $(B64_FAMILY)),true)
+        MSM_VIDC_TARGET_LIST := $(B64_FAMILY)
+        QCOM_HARDWARE_VARIANT := msm8994
+    else
+    ifeq ($(call is-board-platform-in-list, $(BR_FAMILY)),true)
+        MSM_VIDC_TARGET_LIST := $(BR_FAMILY)
+        QCOM_HARDWARE_VARIANT := msm8916
+    else
+    ifeq ($(call is-board-platform-in-list, $(UM_FAMILY)),true)
+        MSM_VIDC_TARGET_LIST := $(UM_FAMILY)
+        QCOM_HARDWARE_VARIANT := msm8937
+    else
+        MSM_VIDC_TARGET_LIST := $(TARGET_BOARD_PLATFORM)
+        QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
+    endif
+    endif
+    endif
     endif
 
-# HACK: check to see if build uses standard QC HAL paths by checking for CM path structure
-AOSP_VARIANT_MAKEFILE := $(wildcard hardware/qcom/audio/default/Android.mk)
-ifeq ("$(AOSP_VARIANT_MAKEFILE)","")
-$(call project-set-path,qcom-audio,hardware/qcom/audio)
-$(call project-set-path,qcom-display,hardware/qcom/display)
-$(call project-set-path,qcom-media,hardware/qcom/media)
-$(call set-device-specific-path,CAMERA,camera,hardware/qcom/camera)
-$(call set-device-specific-path,GPS,gps,hardware/qcom/gps)
-$(call set-device-specific-path,SENSORS,sensors,hardware/qcom/sensors)
-$(call set-device-specific-path,LOC_API,loc-api,vendor/qcom/opensource/location)
-$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
-$(call project-set-path,ril,hardware/ril)
-$(call project-set-path,wlan,hardware/qcom/wlan)
-$(call project-set-path,bt-vendor,hardware/qcom/bt)
-else
 $(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(QCOM_HARDWARE_VARIANT))
-
-ifeq ($(SONY_BF64_KERNEL_VARIANT),true)
-$(call project-set-path,qcom-display,hardware/qcom/display-caf/sony)
-$(call project-set-path,qcom-media,hardware/qcom/media-caf/sony)
-else
 $(call project-set-path,qcom-display,hardware/qcom/display-caf/$(QCOM_HARDWARE_VARIANT))
 $(call project-set-path,qcom-media,hardware/qcom/media-caf/$(QCOM_HARDWARE_VARIANT))
-endif
 
 $(call set-device-specific-path,CAMERA,camera,hardware/qcom/camera)
 $(call set-device-specific-path,GPS,gps,hardware/qcom/gps)
@@ -115,13 +103,12 @@ $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource
 $(call ril-set-path-variant,ril)
 $(call wlan-set-path-variant,wlan-caf)
 $(call bt-vendor-set-path-variant,bt-caf)
-endif # AOSP_VARIANT_MAKEFILE
 
 else
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio/default)
 $(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
-$(call project-set-path,qcom-media,hardware/qcom/media)
+$(call project-set-path,qcom-media,hardware/qcom/media/$(TARGET_BOARD_PLATFORM))
 
 $(call project-set-path,qcom-camera,hardware/qcom/camera)
 $(call project-set-path,qcom-gps,hardware/qcom/gps)
