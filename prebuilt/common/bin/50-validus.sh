4515ddab3a0bccb86445e0c67e830b581075ddc3
Prema Chand Alugu <premaca@gmail.com>
2017-07-11 02:59:54 +0530

    Fix tesla to validus typos

--- prebuilt/common/bin/50-validus.sh
+++ prebuilt/common/bin/50-validus.sh
@@ -1,6 +1,6 @@
 #!/sbin/sh
 # 
-# /system/addon.d/50-tesla.sh
+# /system/addon.d/50-validus.sh
 # During a Slim upgrade, this script backs up /system/etc/hosts,
 # /system is formatted and reinstalled, then the file is restored.
 #
